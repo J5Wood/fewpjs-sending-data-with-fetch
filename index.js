a60049235d4c09a8289d4ead7dd6618e39678954
Jeremy Wood <js.wood206@gmail.com>
2021-03-04 01:08:12 -0800

    commit tree bd2c849d50e90a503d105c4ec4a61e74940d9c6d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function configObj(name, email) { 
+    return {
+        method: "POST",
+        headers: { 
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({ name: name, email: email})
+    }
+};
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", configObj(name, email))
+    .then(resp => resp.json())
+    .then(function(json) {
+        const idNum = document.createElement("p");
+        idNum.innerHTML =json["id"] + " " + json["name"];
+        document.body.appendChild(idNum);
+    })
+    .catch(function(error) {
+        const errorMsg = document.createElement("p");
+        errorMsg.innerHTML = error.message;
+        document.body.appendChild(errorMsg);
+    });
+}
